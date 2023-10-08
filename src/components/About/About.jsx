729705caac14ea7ceaa6ea687098485f5e876326
LHufton <lukehufton@gmail.com>
2023-10-08 18:00:11 -0500

    social icons

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,24 +1,40 @@
 import React from 'react'
 import ProfilePic from '../../images/Profile.jpeg'
 import './About.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const About = () => {
   return (
     <div id="About">
       <h2 className="heading">Luke Hufton</h2>
 
-      <h3>Memphis, TN</h3>
       <h4>Fullstack Software Developer</h4>
+      <h6>Memphis, TN</h6>
       <h5>
         Finding simple solutions <br />
         to complex problems.
       </h5>
       <div className="about-me-container">
-        <div className="about-text">
-          {/* Insert your about me text/content here */}
-        </div>
+        <div className="about-text"></div>
         <div className="profile-container">
           <img id="Profile" src={ProfilePic} alt="Luke Hufton" />
+          <div className="social-links">
+            <a
+              href="https://www.linkedin.com/in/luke-hufton/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+            <a
+              href="https://https://github.com/LHufton"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+          </div>
         </div>
       </div>
     </div>
