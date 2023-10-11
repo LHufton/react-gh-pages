349d22a3f907ffafcdd3b6133e0fc0c0ceff3368
LHufton <lukehufton@gmail.com>
2023-10-11 04:57:35 -0500

    icons debugged

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,98 +2,53 @@ import React from 'react'
 import './Skills.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faJsSquare,
+  faJs,
   faPython,
-  faReact,
-  faAngular,
-  faVuejs,
   faJava,
+  faReact,
+  faVuejs,
+  faNode,
   faBootstrap,
-  faNodeJs,
+  faYarn,
+  faNpm,
+  faMicrosoft,
+  faGitAlt,
   faDocker,
-  faGit,
-  faAws,
-  faMicrosoft
+  faAws
 } from '@fortawesome/free-brands-svg-icons'
-import {
-  faCode,
-  faWind,
-  faTerminal,
-  faDatabase,
-  faServer
-} from '@fortawesome/free-solid-svg-icons'
+import { faDatabase } from '@fortawesome/free-solid-svg-icons'
+
+const skillsData = [
+  { icon: faJs, label: 'JavaScript' },
+  { icon: faPython, label: 'Python' },
+  { icon: faJava, label: 'Java' },
+  { icon: faReact, label: 'React.js' },
+  { icon: faVuejs, label: 'Vue.js' },
+  { icon: faNode, label: 'Node.js' },
+  { icon: faDatabase, label: 'SQL' },
+  { icon: faBootstrap, label: 'Bootstrap' },
+  { icon: faYarn, label: 'Material-UI' },
+  { icon: faNpm, label: 'Tailwind CSS' },
+  { icon: faPython, label: 'Django' },
+  { icon: faMicrosoft, label: '.NET Core' },
+  { icon: faNode, label: 'Express.js' },
+  { icon: faPython, label: 'FastAPI' },
+  { icon: faDatabase, label: 'MySQL' },
+  { icon: faDatabase, label: 'PostgreSQL' },
+  { icon: faDatabase, label: 'MongoDB' },
+  { icon: faDocker, label: 'Docker' },
+  { icon: faGitAlt, label: 'Git' },
+  { icon: faAws, label: 'AWS' }
+]
 
 const Skills = () => {
   return (
     <div id="skills-grid">
-      <div>
-        <FontAwesomeIcon icon={faJsSquare} /> JavaScript
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faPython} /> Python
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faJava} /> Java
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faReact} /> React.js
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faAngular} /> Angular
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faVuejs} /> Vue.js
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faNodeJs} /> Node.js
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faTerminal} /> Shell Scripting
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faDatabase} /> SQL
-      </div>
-
-      <div>
-        <FontAwesomeIcon icon={faBootstrap} /> Bootstrap
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faCode} /> Material-UI
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faWind} /> Tailwind CSS
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faServer} /> Django
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faMicrosoft} /> .NET Core
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faNodeJs} /> Express.js
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faServer} /> FastAPI
-      </div>
-
-      <div>
-        <FontAwesomeIcon icon={faDatabase} /> MySQL
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faDatabase} /> PostgreSQL
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faDatabase} /> MongoDB
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faDocker} /> Docker
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faGit} /> Git
-      </div>
-      <div>
-        <FontAwesomeIcon icon={faAws} /> AWS
-      </div>
+      {skillsData.map((skill) => (
+        <div key={skill.label}>
+          <FontAwesomeIcon icon={skill.icon} /> {skill.label}
+        </div>
+      ))}
     </div>
   )
 }
