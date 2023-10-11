acc0dd3729bddb51f9eacb9408efbd23d0e468b8
LHufton <lukehufton@gmail.com>
2023-10-11 02:00:14 -0500

    content

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -5,17 +5,24 @@ import TicTacToe from '../../images/Tic Tac Toe.png'
 const Project = () => {
   return (
     <div id="Projects" className="background-alt">
-      <h2>Completed ps</h2>
+      <h2>My Work</h2>
       <div className="GA-projects-container grid">
         <div className="simon-project">
           <img className="simon" src={Simon} alt="Simon " />
           <h3>Simon</h3>
-          <p>Description for Simon...</p>
+          <p>
+            I created a digital version of the classic memory game using HTML,
+            CSS and Javascript after two weeks of instruction at General
+            Assembly.
+          </p>
         </div>
         <div className="TTT-project">
           <img className="TTT" src={TicTacToe} alt="Tic Tac Toe" />
           <h3>Tic Tac Toe</h3>
-          <p>Description for Tic Tac Toe...</p>
+          <p>
+            The classic game of tic-tac-toe crated using HTML, CSS and
+            Javascript after one week's instruction at General Assembly.
+          </p>
         </div>
       </div>
     </div>
