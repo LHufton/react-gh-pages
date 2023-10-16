adb12c4b1582844e7404964306e74b6131dda161
LHufton <lukehufton@gmail.com>
2023-10-16 00:56:05 -0500

    bug fixes icon updates

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,6 +6,11 @@ const About = () => {
   return (
     <div id="About">
       <h2 className="heading">About Me</h2>
+      <p>
+        I am a full stack developer who enjoys building MERN stack applications.
+        I am proficient with multiple languages and databases but I thoroughly
+        enjoy working with React.{' '}
+      </p>
       <div className="about-text"></div>
       <div className="about-me-container"></div>
     </div>
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,33 +1,43 @@
-// Skills.jsx
-import React from 'react'
-import './Skills.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDatabase } from '@fortawesome/free-solid-svg-icons'
+import {
+  faJs,
+  faPython,
+  faJava,
+  faReact,
+  faVuejs,
+  faNode,
+  faBootstrap,
+  faYarn,
+  faNpm,
+  faMicrosoft,
+  faGitAlt
+} from '@fortawesome/free-brands-svg-icons'
 
 const categories = {
-  'Languages/Frameworks': [
-    'JavaScript',
-    'Python',
-    'Java',
-    'React.js',
-    'Vue.js',
-    'Node.js'
+  'Languages, Libraries & Frameworks': [
+    { label: 'JavaScript', icon: faJs },
+    { label: 'Python', icon: faPython },
+    { label: 'Java', icon: faJava },
+    { label: 'React.js', icon: faReact },
+    { label: 'Vue.js', icon: faVuejs },
+    { label: 'Node.js', icon: faNode }
   ],
   'DevOps & Backend': [
-    'SQL',
-    'Django',
-    '.NET Core',
-    'Express.js',
-    'FastAPI',
-    'MySQL',
-    'PostgreSQL',
-    'MongoDB',
-    'Docker',
-    'AWS'
+    { label: 'SQL', icon: faDatabase },
+    { label: 'Django', icon: faPython },
+    { label: '.NET Core', icon: faMicrosoft },
+    { label: 'Express.js', icon: faNode },
+    { label: 'FastAPI', icon: faPython },
+    { label: 'MySQL', icon: faDatabase },
+    { label: 'PostgreSQL', icon: faDatabase },
+    { label: 'MongoDB', icon: faDatabase }
   ],
   'Development Tools & Technologies': [
-    'Bootstrap',
-    'Material-UI',
-    'Tailwind CSS',
-    'Git'
+    { label: 'Bootstrap', icon: faBootstrap },
+    { label: 'Material-UI', icon: faYarn },
+    { label: 'Tailwind CSS', icon: faNpm },
+    { label: 'Git', icon: faGitAlt }
   ]
 }
 
@@ -40,8 +50,8 @@ const Skills = () => {
           <h4>{category}</h4>
           <div className="skills-grid">
             {categories[category].map((skill) => (
-              <div key={skill} className="skill-item">
-                {skill}
+              <div key={skill.label} className="skill-item">
+                <FontAwesomeIcon icon={skill.icon} /> {skill.label}
               </div>
             ))}
           </div>
