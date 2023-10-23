f3a126f47fdda86b8fa3c3aaf1d23afbf13ec4a0
LHufton <lukehufton@gmail.com>
2023-10-23 08:32:03 -0500

    more about me content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,8 +5,19 @@ import './About.css'
 const About = () => {
   return (
     <div id="About">
-      <h2 className="heading">What I like to do</h2>
-      <p>I enjoy creating applications to benefit people's lives.</p>
+      <h2>Who I am.</h2>
+      <p>
+        I am an off the beaten path kinda guy who uses out of the box thinking
+        to solve problems. I have an entrepreneurial background and have owned a
+        successful roofing company.
+      </p>
+      <h2>What I like to do</h2>
+      <p>
+        I enjoy a challenge and software engineering provides plenty of them. I
+        am at my best when the problems I am solving have positive impact on our
+        communities.
+      </p>
+
       <div className="about-text"></div>
       <div className="about-me-container"></div>
     </div>
