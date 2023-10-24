67219e51f982f21a3e233d520f971ec3b6c251d2
LHufton <lukehufton@gmail.com>
2023-10-24 01:01:22 -0500

    about content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,15 +14,8 @@ const About = () => {
     <div id="About">
       <h2 className="who">Who I am.</h2>
       <p>
-        I am an off the beaten path kinda guy who uses out of the box thinking
-        to solve problems. I have an entrepreneurial background and have owned a
-        successful roofing company and have found success as a contractor.
-      </p>
-      <h2 className="what">What I like to do</h2>
-      <p>
-        I enjoy a challenge and software engineering provides plenty of them. I
-        am at my best when the problems I am solving have a positive impact on
-        people and enrich their lives.
+        I am a divergent thinker with a passion for mental health and wellness
+        applications. I enjoy solving problems that provide a positive impact.
       </p>
       <span id="to-top" onClick={scrollToTop}>
         <i className="fa fa-chevron-up" aria-hidden="true"></i>
