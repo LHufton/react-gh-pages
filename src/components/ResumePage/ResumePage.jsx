434c59758618bea14b2c389d6d2d0374e51b2ecc
LHufton <lukehufton@gmail.com>
2023-10-29 01:08:17 -0500

    react-router

--- src/components/ResumePage/ResumePage.jsx
+++ src/components/ResumePage/ResumePage.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import Resume from './Resume'
+import '@react-pdf-viewer/core/lib/styles/index.css'
+import { Worker, pdfjs, Document, Page } from '@react-pdf-viewer/core'
+import resumePdf from '../../images/LHuftonResumé.pdf' // Update the path based on your project structure
 
 const ResumePage = () => {
   return (
-    <div>
-      <Resume enableAutoScroll={false} />
+    <div style={{ width: '100%', height: '100vh', backgroundColor: '#f0f0f0' }}>
+      <Worker
+        workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`}
+      >
+        <div style={{ margin: '20px' }}>
+          <Document file={resumePdf}>
+            <Page pageNumber={1} />
+          </Document>
+        </div>
+      </Worker>
     </div>
   )
 }
