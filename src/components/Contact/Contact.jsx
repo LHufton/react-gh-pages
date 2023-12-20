50951d0681f3eeda3fad945e77a491d379aef8c0
LHufton <lukehufton@gmail.com>
2023-12-20 02:58:36 -0600

    footer styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,31 +10,44 @@ const Contact = () => {
   }
   // Formspree contact form
   return (
-    <form id="Contact" onSubmit={handleSubmit}>
-      <label htmlFor="email"></label>
-      <input
-        id="email"
-        type="email"
-        name="email"
-        placeholder="Your email address"
-      />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
+    <div className="container">
+      <form id="Contact" onSubmit={handleSubmit}>
+        <label htmlFor="email"></label>
+        <input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Your email address"
+        />
+        <ValidationError prefix="Email" field="email" errors={state.errors} />
 
-      <label htmlFor="message"></label>
-      <textarea id="message" name="message" placeholder="Enter your message" />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
+        <label htmlFor="message"></label>
+        <textarea
+          id="message"
+          name="message"
+          placeholder="Enter your message"
+        />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
 
-      <button type="submit" disabled={state.submitting}>
-        Submit
-      </button>
-    </form>
+        <button
+          className="contact-sub-button"
+          type="submit"
+          disabled={state.submitting}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   )
 }
 
 const App = () => {
   return (
     <div>
-      <h1 className="contact-heading">Contact me</h1>
       <Contact />
     </div>
   )
