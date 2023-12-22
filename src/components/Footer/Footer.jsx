21362ea41f08b554fe4a504ecc945ffe57aaf9fa
LHufton <lukehufton@gmail.com>
2023-12-22 01:01:24 -0600

    less is more...divs on the footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,14 +4,10 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer>
-      <div className="container">
-        <div className="row">
-          <div className="col-sm-5 copyright">
-            <p>
-              Copyright &copy; <span id="current-year">2023</span> Luke Hufton
-            </p>
-          </div>
-        </div>
+      <div className="copyright">
+        <p>
+          Copyright &copy; <span id="current-year">2023</span> Luke Hufton
+        </p>
       </div>
     </footer>
   )
