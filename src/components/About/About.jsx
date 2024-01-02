2d7ef59568da16fa6f2ca8a057bf51ba7fa7d4e9
LHufton <lukehufton@gmail.com>
2024-01-02 03:37:05 -0600

    about content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,12 +10,13 @@ const About = () => {
         <h2>About me</h2>
 
         <p>
-          Hello! My name is Luke and I am a former entrepreneur who found his
-          way to software development. I am passionate about solving problems by
-          finding simple and effective solutions. When I am not creating and
-          maintaining applications you can find me hiking with my dog Kimber,
-          playing chess or reading. I am currently seeking a position as an
-          associate software developer.
+          Hello! My name is Luke and I am a former entrepreneur <br />
+          who found his way to software development. I enjoy thinking outside of
+          the box to find solutions that are simple to understand. <br /> When I
+          am not creating and maintaining applications you can find me hiking
+          with my dog Kimber, playing chess or reading.
+          <br /> I am currently seeking a position as an associate software
+          developer.
         </p>
       </div>
       <div className="dog-container">
