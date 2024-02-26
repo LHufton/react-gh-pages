e7275749b4fb730306a32e45c1714d35500f34c4
LHufton <lukehufton@gmail.com>
2024-02-25 22:52:54 -0600

    about component  grammar changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,11 +11,11 @@ const About = () => {
 
         <p>
           Hello! My name is Luke, born in England and living in America. When I
-          am not creating new applications you can find me with my dog, Kimber,
-          hiking a trail. I enjoy playing chess, listening to music, reading and
-          playing video games. <br />
+          am not creating new applications, you can find me with my dog, Kimber,
+          hiking a trail. I enjoy playing chess, listening to music, reading,
+          and playing video games. <br />
           <br />I would like to work for a company that believes in bettering
-          our local communities and developing products to assist in people's
+          our local communities and developing products to assist people's
           lives. <br />
           <br /> I am currently seeking a position as an associate software
           developer.
